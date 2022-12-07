3e2d3ad8a2ea2615d264e0f6389524f221f060fc
Kilian <57487634+qualitycont@users.noreply.github.com>
2022-12-07 09:37:52 +0100

    render function

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import subprocess
 
 MULTICAST_GROUP = '224.11.154.1'
 PORT = 22333
@@ -10,5 +11,8 @@ cl = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 def send(msg: bytes):
     print(f"Sending {str(msg)}...")
     cl.sendto(msg, (MULTICAST_GROUP, PORT))
+
+def render(path: str, startFrame: int, endFrame: int):
+    subprocess.run(f"blender -ba -f {startFrame}..{endFrame} {path}")
 
 send(b"OK Server")
