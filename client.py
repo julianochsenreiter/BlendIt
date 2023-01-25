b7b24845cebd754b0185f423d7d29d5f34bfe895
Kilian <noqualitycont@gmail.com>
2023-01-25 09:51:14 +0100

    Improve code structure

--- client.py
+++ client.py
@@ -2,38 +2,83 @@ import socket
 import os 
 import subprocess
 import sys
+from typing import Tuple
 
+"""
+    INIT
+"""
+
+# Connection info
 MULTICAST_GROUP = '224.11.154.1'
 PORT = 22333
-mount_point = "/etc/nfs"
 hostname = socket.gethostname()
 ipaddr = socket.gethostbyname(hostname)
-file = "*.blend"
-
-
-# Time to live for packet
 TTL = 2
+
+# Mount info
+mount_point = "/etc/nfs"
+file_extension = "*.blend"
+
+# Create socket
 cl = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-def send(msg: bytes):
-    print(f"Sending {str(msg)}...")
-    cl.sendto(msg, (MULTICAST_GROUP, PORT))
+"""
+    FUNCTIONS
+"""
+# Util functions
+def log(msg: str):
+    print(msg)
 
-send(b"OK Server")
+# Socket functions
+def send(msg: bytes):
+    log(f"Sending {str(msg)}...")
+    cl.sendto(msg, (MULTICAST_GROUP, PORT))
 
 def receivefile():
     subprocess.run(["mount", ipaddr + ":" + mount_point])
     # if os.path.exists(mount_point + "/" + file):
     #     subprocess.run(["cp", mount_point + "/" + file, file])
 
-def receive() -> bytes:
-    return cl.recv(32)
+def receive() -> Tuple[bytes,str]:
+    return cl.recvfrom(512)
 
+# Render functions
 def render(path: str, startFrame: int, endFrame: int):
     subprocess.run(f"blender -b {path} -o //render_ -f {startFrame}..{endFrame} -F PNG -x 1 ")
 
-send(b"OK Server")
 
-while True:
-    msg = receive()
+"""
+    MAIN LOOP
+"""
+
+# Server information
+server_address = ""
+file_name = ""
+frame_start = 0
+frame_end = 0
+
+wantsToExit = False
+
+while not wantsToExit:
+    if len(server_address) == 0:
+        send(b"Hello")                
+        msg, sender = receive()
+        server_address = sender
+        file_name = str(msg)
+
+    if frame_start != frame_end:
+        render(mount_point + file_name, frame_start, frame_end)
+        send(b"DONE")
+        
+        frame_start = 0
+        frame_end = 0
+
+    msg, sender = receive()
+    if sender != server_address:
+        log("Other server tried to contact during session!")
+        continue
+    
+    parts = str(msg).split(';')
+    frame_start = int(parts[0])
+    frame_end = int(parts[1])
     
--- server.py
+++ server.py
@@ -5,15 +5,23 @@ import subprocess
 import sys
 import pyfiglet
 
+"""
+    INIT
+"""
+
+# Setup connection Info
 MULTICAST_GROUP = '224.11.154.1' # 224.B.l.1
 PORT = 22333
-mount_point = "/mnt/nfs"
 
+# Setup mount info
+mount_point = "/mnt/nfs"
+FILE_PATH = sys.argv[1]
+
+# Create socket
 serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
 serv.bind(('', PORT))
-
+# Enable multicast
 mreq = struct.pack("4sl", socket.inet_aton(MULTICAST_GROUP), socket.INADDR_ANY) # turn this into bytes
 serv.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
@@ -22,6 +30,10 @@ print(WELCOME_TEXT)
 print("BlendIT server has started...")
 
 registered_clients = list()
+
+"""
+    FUNCTIONS
+"""
 
 def handleMessage(msg, sender):
     cl = sender[0] # get IP address from sender tuple
@@ -36,7 +48,12 @@ def handleMessage(msg, sender):
 def transferFile():
     for client in registered_clients:
         subprocess.run(["mount", client + ":" + mount_point])
-        subprocess.run(["cp", sys.argv[1], mount_point])
+        subprocess.run(["cp", FILE_PATH, mount_point])
+
+"""
+    MAIN LOOP
+"""
+
 while True:
     handleMessage(serv.recvfrom(1024))
     transferFile()
